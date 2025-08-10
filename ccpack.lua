cf9d38457d4f5d72ff9557df2d15050ada39285b
Andrei Aldea <aldea.andrei.977@gmail.com>
2025-08-11 01:15:50 +0300

    repo: first attempt at this thing

---
+++ ccpack.lua
@@ -0,0 +1,100 @@
+--[[
+    CCPack Lua script for packaging a project for use with ComputerCraft
+
+    This script is designed to be run in a ComputerCraft environment.
+
+    Usage:
+    1. wget https://raw.githubusercontent.com/Al-Andrew/ccpack/main/ccpack.lua
+    2. ccpack.lua <package_you_want_installed>
+
+    This the package name will be something like "Al-Andrew/cc-afk" (it should be hosted on GitHub)
+    This script will look for a baked_manifest.json file in the root of the repository.
+    If it exists, it will use that to install the package.
+
+    The files get installed in a directory named after the package in the current working directory.
+--]]
+
+
+---@class BakedFile
+---@field path string The path to the file relative to the package root
+---@field url string The URL to download the file from
+
+---@class BakedManifest
+---@field name string The name of the package
+---@field version string The version of the package
+---@field url string The URL to download the package from
+---@field files table<BakedFile> A list of files to download for the package
+
+
+--- Does a http get on the url looking for a baked_manifest.json file
+---@param package_name string The name of the package
+---@return BakedManifest The parsed manifest object
+local function get_manifest(package_name)
+    local url = "https://raw.githubusercontent.com/" .. package_name .. "/main/baked_manifest.json" -- toodo: support other branches/versions
+    local response = http.get(url)
+    if not response then
+        error("Failed to download manifest from " .. url)
+    end
+
+    ---@type string
+    local content = response.readAll()
+    response.close()
+    
+    return textutils.unserializeJSON(content)
+end
+
+
+--- Creates a directory if it does not exist, including parent directories
+--- @param path string The path to the directory to create
+local function mkdir_if_not_exists_with_parent(path)
+    if fs.exists(path) then
+        return
+    end
+    
+    local parent = fs.getDir(path)
+    if not fs.exists(parent) then
+        mkdir_if_not_exists_with_parent(parent)
+    end
+    fs.makeDir(path)
+end
+
+--- Downloads a file from the given URL and saves it to the specified path
+---@param path string The path to save the file to
+---@param url string The URL to download the file from
+local function download_file_to_path(path, url)
+    mkdir_if_not_exists_with_parent(fs.getDir(path))
+    
+    local response = http.get(url)
+    if not response then
+        error("Failed to download file from " .. url)
+    end
+
+    local file = fs.open(path, "wb")
+    if not file then
+        error("Failed to open file for writing: " .. path)
+    end
+
+    file.write(response.readAll())
+    file.close()
+    response.close()
+end
+
+--- Installs a package by downloading its files according to the manifest
+---@param package_name string The name of the package to install
+local function install_package(package_name)
+    local manifest = get_manifest(package_name)
+    if not manifest or not manifest.files then
+        error("Invalid manifest for package: " .. package_name)
+    end
+
+    for _, file in ipairs(manifest.files) do
+        download_file_to_path(file.path, file.url)
+    end
+end
+
+
+--- Driver code
+local package_name = arg[1]
+if not package_name then
+    error("Usage: ccpack.lua <package_name>")
+end
---
+++ ccpack.py
@@ -0,0 +1,47 @@
+"""
+This is the local component for ccpack.
+You can use this to create the baked_manifest.json file needed for ccpack to work.
+
+This requries a local file with the name ccpack.json to be present in the same directory as this file.
+This file should contain a JSON object with the following structure:
+{
+    "name": "ccpack",
+    "version": "1.0.0",
+    "author": "Your Name",
+}
+
+This script will look through the current directory recursively and create a baked_manifest.json file
+with all the lua files found.
+"""
+
+import os
+import json
+
+
+def create_baked_manifest():
+    # Load the ccpack.json file
+    with open('ccpack.json', 'r') as f:
+        ccpack_data = json.load(f)
+
+    # Create the baked_manifest.json structure
+    baked_manifest = {
+        "name": ccpack_data["name"],
+        "version": ccpack_data["version"],
+        "author": ccpack_data["author"],
+        "files": []
+    }
+
+    # Walk through the current directory recursively
+    for root, dirs, files in os.walk('.'):
+        for file in files:
+            if file.endswith('.lua'):
+                file_path = os.path.join(root, file)
+                baked_manifest["files"].append(file_path)
+
+    # Write the baked_manifest.json file
+    with open('baked_manifest.json', 'w') as f:
+        json.dump(baked_manifest, f, indent=4)
+
+if __name__ == "__main__":
+    create_baked_manifest()
+    print("baked_manifest.json created successfully.")
